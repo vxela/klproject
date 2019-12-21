05475e075774d9e33f6270ce7a9a68416f579797
mush60 <mushti8@gmail.com>
2019-12-22 05:11:27 +0700

    creating portal view

---
+++ resources/views/portal/layout/main.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>
+
+      @yield('page_title')
+
+  </title>
+
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  @yield('meta_link_css_etc')
+
+</head>
+
+<body>
+  <!--==========================
+  Header
+  ============================-->
+  <header id="header">
+
+    @yield('topbar_header')
+    
+
+    <div class="container">
+
+        @yield('logo')
+
+        @yield('navigation')
+      
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  @yield('s_intro')
+
+
+  <main id="main">
+
+    @yield('s_info')
+
+
+    @yield('s_varietas')
+
+    @yield('s_awards')
+
+    @yield('s_faq')
+
+  </main>
+
+    @yield('footer')
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <!-- Uncomment below i you want to use a preloader -->
+  <!-- <div id="preloader"></div> -->
+
+  <!-- JavaScript Libraries -->
+  @yield('src_js')
+
+</body>
+</html>
