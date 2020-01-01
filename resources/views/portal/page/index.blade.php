1fedd1a6c93368dbc61854972fb7f64fef51c5be
mush60 <mushti8@gmail.com>
2020-01-01 14:27:24 +0700

    remove icon page

--- resources/views/portal/page/index.blade.php
+++ resources/views/portal/page/index.blade.php
@@ -6,8 +6,8 @@
 
 @section('meta_link_css_etc')
     <!-- Favicons -->
-    <link href="{{asset('/dist/img/favicon.png')}}" rel="icon">
-    <link href="{{asset('/dist/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+    {{-- <link href="{{asset('/dist/img/favicon.png')}}" rel="icon">
+    <link href="{{asset('/dist/img/apple-touch-icon.png')}}" rel="apple-touch-icon"> --}}
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,600,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet">
