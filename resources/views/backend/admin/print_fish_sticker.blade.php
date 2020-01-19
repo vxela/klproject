7d92c2027caeba9fe007dea0c69f9ca42f8d690d
mush60 <mushti8@gmail.com>
2020-01-19 20:12:55 +0700

    masih kacau

--- resources/views/backend/admin/print_fish_sticker.blade.php
+++ resources/views/backend/admin/print_fish_sticker.blade.php
@@ -6,37 +6,65 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
+    body {
+        width: 650px;
+    }    
     img {
         border: 1px solid #ddd;
         border-radius: 4px;
         padding: 5px;
-        width: 90;
+        width: 70;
+        height: 100;
     }
-    /* .main {
-        width: 100%;
-        overflow: hidden;
+    .main {
+        width: 650px;
+        style="overflow: hidden;
     }
-    .tab_item {
-        display: inline;
-    } */
+    .tab {
+        width: 200px;
+        display: inline-block;
+        border: #000 1px solid;
+        /* float: left; */
+    }
+    .bx {
+        width: 50%;
+    }
     </style>
 </head>
 <body>
     <div class="main">
+        @php
+            $n = 1;
+        @endphp
         @foreach ($data_stc as $stc)
-            <table width="200" class="tab_item">
-                <tr>
-                    <td rowspan="2">
-                    <img src="{{substr($stc->fish_picture, 1)}}" alt="Paris" style="width:90px">                
-                    </td>
-                    <td>ID</td>
-                    <td>{{Mush::no_reg($stc->id)}}</td>
-                </tr>
-                <tr>
-                    <td>Size</td>
-                    <td>{{$stc->fish_size}}</td>
-                </tr>
-            </table>
+            <div class="tab">
+                <div class="img bx">
+                    <img src="{{substr($stc->fish_picture, 1)}}">
+                </div>
+                <div class="bx">
+                    <div>
+                        <div>d</div>
+                        <div>s</div>
+                    </div>
+                    <div>
+                        <div>x</div>
+                        <div>y</div>
+                    </div>
+                </div>
+                    {{-- <table width="100%" class="tab_item">
+                        <tr>
+                            <td rowspan="2">
+                            <img src="{{substr($stc->fish_picture, 1)}}">
+                            </td>
+                            <td>ID</td>
+                            <td>{{Mush::no_reg($stc->id)}}</td>
+                        </tr>
+                        <tr>
+                            <td>Size</td>
+                            <td>{{$stc->fish_size}}</td>
+                        </tr>
+                    </table> --}}
+            </div>
         @endforeach
     </div>
 </body>
