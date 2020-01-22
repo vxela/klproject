d001d4346813c4de785cb70820d4366989c975b4
mush60 <mushti8@gmail.com>
2020-01-23 03:16:46 +0700

    fixing page break

--- resources/views/backend/admin/print_user_fish_data.blade.php
+++ resources/views/backend/admin/print_user_fish_data.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$user->bio->nama}} Fish Data</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://printjs-4de6.kxcdn.com/print.min.css">
     <style>
     img {
         border: 1px solid #ddd;
@@ -13,13 +14,20 @@
         padding: 5px;
         max-width: 100%;
     }
+    @media print {
+        body{ overflow:visible; }
+        .newpage {
+            display: block;
+            page-break-after: always;
+        }
+    }
     </style>
 </head>
 <body>
     <div class="container">
         <h2 class="mb-3">Koi Lovers Makassar Project | Data Ikan : {{$user->bio->nama}}</h2>
         <div class="col-md-12 mt-3">
-            <div class="row">
+            <div class="row prit">
                 @php
                     $n=1;
                 @endphp
@@ -62,7 +70,20 @@
                         <small>
                             {{$fish->bio->kota}}
                         </small><br><hr class="m-0">
-                    </div>            
+                    </div>
+
+            @if ($loop->iteration % 6 == 0)
+                </div>
+                <div class="newpage"></div>  
+                <div class="row">
+            @endif
+            {{-- @if ($n%6 == 0)
+                </div>
+                xxxxx
+                <div class="row">
+            @elseif($n == count($fishs))
+                </div>
+            @endif --}}
                 @endforeach
                 {{-- <div class="col-3">
                     data
@@ -76,6 +97,7 @@
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://printjs-4de6.kxcdn.com/print.min.js"></script>
 <script>
     $(document).ready(function(){
         // $.ajaxSetup({ cache: false });
