655287199eaf2fc9f891e880c356b42f12e7b2ef
mush60 <mushti8@gmail.com>
2020-01-28 14:18:57 +0700

    modif dashboard index

--- resources/views/backend/admin/index.blade.php
+++ resources/views/backend/admin/index.blade.php
@@ -33,7 +33,7 @@
             <div class="d-flex border">
                 <div class="bg-dark text-light p-4">
                     <div class="d-flex align-items-center h-100">
-                        <i class="fa fa-3x fa-fw fa-users"></i>
+                        <i class="fa fa-3x fa-fw fa-user"></i>
                     </div>
                 </div>
                 <div class="flex-grow-1 bg-white p-4">
@@ -59,7 +59,7 @@
             <div class="d-flex border">
                 <div class="bg-success text-light p-4">
                     <div class="d-flex align-items-center h-100">
-                        <i class="fa fa-3x fa-fw fa-users"></i>
+                        <i class="fa fa-3x fa-fw fa-check"></i>
                     </div>
                 </div>
                 <div class="flex-grow-1 bg-white p-4">
@@ -72,7 +72,7 @@
             <div class="d-flex border">
                 <div class="bg-warning text-light p-4">
                     <div class="d-flex align-items-center h-100">
-                        <i class="fa fa-3x fa-fw fa-users"></i>
+                        <i class="fa fa-3x fa-fw fa-clock-o"></i>
                     </div>
                 </div>
                 <div class="flex-grow-1 bg-white p-4">
@@ -88,7 +88,7 @@
             <div class="d-flex border">
                 <div class="bg-primary text-light p-4">
                     <div class="d-flex align-items-center h-100">
-                        <i class="fa fa-3x fa-fw fa-users"></i>
+                        <i class="fa fa-3x fa-fw fa-money"></i>
                     </div>
                 </div>
                 <div class="flex-grow-1 bg-white p-4">
@@ -101,7 +101,7 @@
             <div class="d-flex border">
                 <div class="bg-success text-light p-4">
                     <div class="d-flex align-items-center h-100">
-                        <i class="fa fa-3x fa-fw fa-users"></i>
+                        <i class="fa fa-3x fa-fw fa-money"></i>
                     </div>
                 </div>
                 <div class="flex-grow-1 bg-white p-4">
@@ -114,7 +114,7 @@
             <div class="d-flex border">
                 <div class="bg-warning text-light p-4">
                     <div class="d-flex align-items-center h-100">
-                        <i class="fa fa-3x fa-fw fa-users"></i>
+                        <i class="fa fa-3x fa-fw fa-money"></i>
                     </div>
                 </div>
                 <div class="flex-grow-1 bg-white p-4">
