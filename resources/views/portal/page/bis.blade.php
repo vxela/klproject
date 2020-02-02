faaeb8b9c978383722136a29068566be61243a01
leh-dev <mushti8@gmail.com>
2020-02-03 02:31:27 +0700

    best in size

--- resources/views/portal/page/bis.blade.php
+++ resources/views/portal/page/bis.blade.php
@@ -21,12 +21,15 @@
         </div>
     </div>
     <div class="row">
+        @php
+            $n = 1;
+        @endphp
         @foreach ($data_bis as $bis)
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                 <div class="card">
                     <img class="card-img-top" src="{{$bis->fish_picture}}" alt="Card image cap" style="height: 300px; width: auto;">
                     <div class="card-body text-center p-1">
-                        <h5 class="card-title m-1">#n REGID</h5>
+                        <h5 class="card-title m-1">#{{$n++}} REGID</h5>
                         <p class="card-text mb-1">Fish Variety, Size</p>
                         <p class="card-text mb-1">Fish Owner, Asal</p>
                         <p class="card-text mb-1">Fish Handler, Asal</p>
