869beefdacc1a305e476131151ccc1abc6d128aa
leh-dev <mushti8@gmail.com>
2020-02-07 01:38:41 +0700

    grade champion

--- resources/views/portal/page/regular.blade.php
+++ resources/views/portal/page/regular.blade.php
@@ -44,7 +44,7 @@
         @php
             $n = 1;
         @endphp
-        @foreach ($data_bis as $bis)
+        @foreach ($data_champion as $bis)
             <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mt-2 mb-2">
                 <div class="card">
                     <img class="card-img-top" src="{{$bis->user_fish->fish_picture}}" alt="Card image cap" style="height: 300px; width: auto;">
