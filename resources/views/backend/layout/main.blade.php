46194f60a547800c56eb210f62442bc501af010f
leh-dev <mushti8@gmail.com>
2020-02-07 01:12:34 +0700

    grade champion

--- resources/views/backend/layout/main.blade.php
+++ resources/views/backend/layout/main.blade.php
@@ -48,7 +48,7 @@
                     @yield('pagemenu')
 
                     <li><a href="{{route('admin.fish_point')}}"><i class="fa fa-fw fa-diamond"></i> Fish Points</a></li>
-                    <li><a href="{{route('admin.champion')}}"><i class="fa fa-fw fa-trophy"></i> Fish Champion</a></li>
+                    {{-- <li><a href="{{route('admin.champion')}}"><i class="fa fa-fw fa-trophy"></i> Fish Champion</a></li> --}}
                     <li><a href="{{route('admin.grade')}}"><i class="fa fa-fw fa-trophy"></i> Grade Champion</a></li>
                     <li><a href="{{route('admin.regular_champion')}}"><i class="fa fa-fw fa-flag"></i> Regular Champion</a></li>
                     <li><a href="{{route('admin.best_in_size')}}"><i class="fa fa-fw fa-sort-amount-desc"></i> Best In Size</a></li>
