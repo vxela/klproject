b962c4a6d16358a37b7c23084d7d2590e7fc254d
leh-dev <mushti8@gmail.com>
2020-02-07 15:03:50 +0700

    grade champion

--- resources/views/portal/page/champion.blade.php
+++ resources/views/portal/page/champion.blade.php
@@ -18,7 +18,7 @@
 @endsection
 
 @section('content')
-    <div class="row mb-3">
+    <div class="row justify-content-md-center mb-3">
         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
         </div>
         
